e49df270d18c909f06539d6ab8a6d93f8da029bd
Anders Blom <anders@andersblom.dk>
2017-09-12 13:48:29 -0400

    showing id-param for development purposes

--- src/components/SingleGistShow/SingleGistShow.js
+++ src/components/SingleGistShow/SingleGistShow.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 export default class SingleGistShow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            singleGistToShow: {}
+        }
+
+        console.log(this.props);
+    }
     render() {
         return (
             <div>
-                Single Gist component
+                Single Gist: {this.props.match.params.id}
             </div>
         );
     }
